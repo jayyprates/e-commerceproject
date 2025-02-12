fd3418d4755ce1549fc19401f499f0f7b9eee42d
jayyprates <joanaprates2@gmail.com>
2025-02-12 18:10:47 -0300

    Initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+export function Header () {
+	return (
+		<nav>
+			<h1>E-commerce CD</h1>
+			<img />
+			<ul>
+				<li>Home</li>
+				<li>Nacionais</li>
+				<li>Interacionais</li>
+				<li>Vinis</li>
+			</ul>
+		</nav>
+	)
+
+}
